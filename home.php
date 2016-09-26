9aba52b004774dd5f93210d959131c0f23b34bc1
Marcos <msanchez@qdqmedia.com>
2016-09-26 13:10:58 +0200

    menu principal & header terminado

--- home.php
+++ home.php
@@ -8,10 +8,10 @@
 <body>
 	<header>
 		<div class="wrapper">
-			<img src="" alt="La agencia de marketing online de las pymes: QDQ media">
+			<img src="http://localhost:8888/QDQmedia/img/logo_qdqmedia_grupo_solocal.png" alt="La agencia de marketing online de las pymes: QDQ media">
 			<nav>
 				<ul>
-					<li><a href="">Inicio</a></li>
+					<li class="menu_item_current"><a href="">Inicio</a></li>
 					<li><a href="">Quienes somos</a></li>
 					<li><a href="">Servicios</a></li>
 					<li><a href="">Blog</a></li>
