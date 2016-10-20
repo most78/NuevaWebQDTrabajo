5d5303b6f27a6fb8a53c7ac33d8cceb9a4b1eeb0
most78 <marcos.ostos@gmail.com>
2016-10-20 18:02:12 +0200

    Producto casi terminado falta pequeños ajustes y la curva

--- home.php
+++ home.php
@@ -26,28 +26,42 @@
 		</div>
 	</section>
 	<section class="productosBox">
-		<div class="wrapper macBackground">
-			<h1 class="tituloSeccionProducto">Descubre qué podemos hacer por tu negocio</h1>
-			<p class="subTituloSeccionProducto">somos la mayor agencia de marketing online para los negocios</p>
+		<div class="wrapper">
+			<div class="macBackground">
+				<h1 class="mainClaim">Descubre qué podemos hacer por tu negocio</h1>
+				<p class="secondaryClaim">somos la mayor agencia de marketing online para los negocios</p>
+				<a href="" class="button">nuestros servicios</a>
+			</div>
 			<div class="contenedorProducto">
 				<div class="producto">
-					<h3 class="tituloProducto">Diseñamos y gestionamos la web de tu negocio</h3>
-					<p>Mas de 20.000 clientes confían en nosotros.
-				   Nuestras páginas web están optimizadas para maximizar la conversión y el retorno de la inversión</p>
+					<span class="circle"></span>
+					<div class="textoProducto">
+						<h3 class="mainClaim">Diseñamos y gestionamos la web de tu negocio</h3>
+						<p>Mas de 20.000 clientes confían en nosotros.
+				   		Nuestras páginas web están optimizadas para maximizar la conversión y el retorno de la inversión</p>
+				   	</div>
 				</div>
 				<div class="producto">
-					<h3 class="tituloProducto">Garantizamos tu posicionamiento en los principales buscadores</h3>
-					<p>95% de nuestras campañas están entre las primeras posiciones (SEO)</p>
+					<span class="circle"></span>
+					<div class="textoProducto">
+						<h3 class="mainClaim">Garantizamos tu posicionamiento en los principales buscadores</h3>
+						<p>95% de nuestras campañas están entre las primeras posiciones (SEO)</p>
+					</div>
 				</div>
 				<div class="producto">
-					<h3 class="tituloProducto">Garantizamos tráfico a tu web por muy poca inversión</h3>
-					<p>Gestionamos más de 18.000 campañas en Google AdWords y medios online</p>
+				<span class="circle"></span>
+					<div class="textoProducto">
+						<h3 class="mainClaim">Garantizamos tráfico a tu web por muy poca inversión</h3>
+						<p>Gestionamos más de 18.000 campañas en Google AdWords y medios online</p>
+					</div>
 				</div>
 				<div class="producto">
-					<h3 class="tituloProducto">Gestión integral y personalizada</h3>
-					<p>Nos ocupamos de gestionar tu estratégia de marketing digital con un trato personalizado, para que tú te encargues de tu negocio. Somos más de 400 profesionales que te ayudamos a crecer</p>
+				<span class="circle"></span>
+					<div class="textoProducto">
+						<h3 class="mainClaim">Gestión integral y personalizada</h3>
+						<p>Nos ocupamos de gestionar tu estratégia de marketing digital con un trato personalizado, para que tú te encargues de tu negocio. Somos más de 400 profesionales que te ayudamos a crecer</p>
+					</div>
 				</div>
-				<a href="" class="button">nuestros servicios</a>
 			</div>
 		</div>
 	</section>
@@ -56,12 +70,6 @@
 			<h2 class="mainClaim">Tendencias de marketing online</h2>
 			<p class="secondaryClaim">El blog de QDQMEDIA, activa internet</p>
 		</div>
-		<!--<ul>
-			<li><a href="">Qdq media participa en la sexta edición de bizbarcelona</a></li>
-			<li><a href="">Test A/B y mapas de calor: El camino hacia la nueva web evolutiva</a></li>
-			<li><a href="">Cómo aumentar el nivel de satisfaccion del usuario</a></li>
-			<li><a href="">QDQ media participa en la sexta edicion de Bizbarcelona</a></li>
-		</ul>-->
 		<ul class="lastPostContainer">
 		<?php
   			$xml = new DomDocument();
