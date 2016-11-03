1a93a2b08f689929b93fe1bb68160f5b701e190e
Mug78 <marcos.ostos@gmail.com>
2016-11-03 18:28:19 +0100

    Socios terminado

---
+++ socios-qdqmedia.php
@@ -0,0 +1,63 @@
+<?php include ("head.php"); ?>
+<?php include("header.php"); ?>
+<?php global $HeaderTitle;  ?> 
+<?php $HeaderTitle = "quiénes somos";  ?>
+<article class="sobreWrapper">
+	<?php include("innerHeaderBackground.php"); ?>
+	<section class="wrapper">
+		<h1 class="mainClaim">Nuestros socios</h1>
+		<p>Enfocados en optimizar y ofrecer las mejores soluciones para nuestros clientes, QDQ media trabaja asociada con otras empresas punteras y de gran peso en el mundo de la publicidad dentro del ámbito de internet. Con estas asociaciones la agencia de marketing online para pymes ha conseguido desarrollar y ofrecer un servicio optimizado y diferenciado a sus clientes.</p>
+	</section>
+	<section class="wrapper">
+		<ul class="empresasGrupo">
+			<li class="empresa">
+				<h2 class="mainClaim"> Socio Premium de Google™</h2>
+				<div class="boxEmpresa">
+					<img src="img/socioPremiumGoogle.png" alt="">
+					<p>Mediante un acuerdo firmado en el 2008, QDQ media se convertía en <a href="" alt="">Socio Premium de Google™</a> para España y Portugal para continuar avanzando en su meta de ayudar a las pequeñas y medianas empresas a gestionar sus campañas en el buscador. Con este acuerdo se incorporó a los servicios que ofrecíamos a nuestros clientes la gestión de Google Adwords™, de modo que se produce un incremento notable en la conversión a través de la web. <a href="" alt="">Ver aviso de divulgación "Cómo trabajar con un socio"</a></p>
+					<div class="buttonContainer">
+						<a href="" class="button">Premier Google partner</a>
+						<a href="" class="button">ver video</a>
+						<a href="" class="button fullWidth">VER LA POLÍTICA DE TERCEROS DE GOOGLE™</a>
+					</div>
+				</div>
+			</li>
+			<li class="empresa">
+				<h2 class="mainClaim">11870.com</h2>
+				<div class="boxEmpresa">
+					<img src="img/11870.png" alt="">
+					<p>Tras llegar a un acuerdo en 2011, QDQ media pasa a incluir los <a href="" alt="">servicios de 11870.com</a>, la página web de recomendaciones de negocios más importante de España, entre sus productos. 
+					Añadiendo, así, a su cartera una nueva herramienta para optimizar la presencia online de las pymes españolas. A través de este acuerdo la visibilidad de nuestros clientes en la red aumentó, todavía más, por las recomendaciones de los usuarios en la plataforma de 11870.com.</p>
+					<a href="" class="button">11870.com</a>
+				</div>
+			</li>
+			<li class="empresa">
+				<h2 class="mainClaim">pan</h2>
+				<div class="boxEmpresa">
+					<img src="img/pan.png" alt="">
+					<p>QDQ media establece una alianza estratégica con PAN, la red de publicidad segmentada en soportes Premium con mayor cobertura en España, gracias a sus colaboraciones con los mayores Grupos Editoriales Españoles.
+					Con este acuerdo QDQ media añade publicidad digital display a su cartera de negocios, acercando a las pymes un mercado publicitario de calidad en importantes soportes editoriales online, maximizando las inversiones del cliente y añadiendo notoriedad y visibilidad a sus negocios.
+					</p>
+					<div class="buttonContainer">
+						<a href="" class="button">pan</a>
+						<a href="" class="button">ver video</a>
+					</div>
+				</div>
+			</li>
+			<li class="empresa">
+				<h2 class="mainClaim">bing</h2>
+				<div class="boxEmpresa">
+					<img src="img/bing.png" alt="">
+					<p>QDQ media firma en 2014 un acuerdo de colaboración con Microsoft Advertising, líder mundial en tecnología, para añadir los buscadores Bing y Yahoo! Search a su cartera de productos, con el objetivo de ayudar a las marcas a conectar con sus consumidores. Ser partner de Microsoft Bing supone una apuesta por seguir ofreciendo un servicio integral y completo a las pymes, ampliando de manera cualitativa las oportunidades de publicidad en buscadores.</p>
+					<div class="buttonContainer">
+						<a href="" class="button">microsoft advertising</a>
+						<a href="" class="button">ver video</a>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</section>
+</article><!-- fin sobreWrapper -->
+<?php include("footer.php"); ?>
+</body>
+</html>
