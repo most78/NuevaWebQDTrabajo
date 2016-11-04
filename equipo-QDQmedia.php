ff3cf1325e778faef451e8062208eb40f44beab0
most78 <marcos.ostos@gmail.com>
2016-11-04 14:44:31 +0100

    socios, equipo e historia terminado

---
+++ equipo-QDQmedia.php
@@ -0,0 +1,21 @@
+<?php include ("head.php"); ?>
+<?php include("header.php"); ?>
+<?php global $HeaderTitle;  ?> 
+<?php $HeaderTitle = "quiénes somos";  ?>
+<article class="sobreWrapper historiaSection">
+	<?php include("innerHeaderBackground.php"); ?>
+	<section class="wrapper">
+		<h1 class="mainClaim">nuestro equipo</h1>
+		<p>Alrededor de 350 personas forman parte del equipo de QDQ media, repartidas por casi todas las ciudades de España, con oficinas en Madrid, Barcelona y Zaragoza.
+		Somos un equipo que se caracteriza por los valores de solidaridad, complementariedad y complicidad, pero sobre todo, un equipo con un gran espíritu innovador y que se adapta rápidamente a los cambios que exige el ser una empresa de Internet.
+		Si quieres formar parte de nuestro equipo puedes ponerte en contacto con nosotros o conocer las ofertas de empleo que publicamos periódicamente en la ficha de QDQ media en Infojobs, QDQ media en Infoempleo y en la sección “trabaja con nosotros” de Solocal.es</p>
+		<figure class="leftSide"><img src="img/equipo.png"><figcaption>trabajadores en la sede de QDQ media en Madrid</figcaption></figure>
+		<p>Over 92% of computers are infected with Adware and spyware. Such software is rarely accompanied by uninstall utility and even when it is it almost always leaves broken Windows Registry keys behind it. Even if you have an anti-spyware tool your Windows Registry might be broken – developers of those tools are focused on removing Adware and spyware functionality, not every trace of software itself.</p>
+		<p>Another category of software that is known to leave bits and pieces behind on uninstallation is games. There are a lot of special installation systems that creates strange files, unique entries in your registry file as well as changes system dll’s to other versions. Once you uninstall the game a lot of the special installation changes it made will be left behind, causing you problems.</p>
+		<p>You should run an advanced registry cleaner for Windows that allows you to safely scan, clean, and repair registry problems with a few simple mouse clicks on a regular basis.</p>
+		<p>Problems with the Windows Registry are a common cause of Windows crashes (blue screens) and error messages (memory as well as disk errors). By using a PC tune-up utility, your system will be more stable, your software will run quicker, and your operating system will boot faster.</p>
+	</section>
+</article><!-- fin sobreWrapper -->
+<?php include("footer.php"); ?>
+</body>
+</html>
